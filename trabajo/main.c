e88c427bacf9aff5bbd545db616bfb301772259a
diego <79934958+diego22222@users.noreply.github.com>
2022-05-06 23:56:32 +0200

    Inicio trabajo tablero

--- trabajo/main.c
+++ trabajo/main.c
@@ -1,61 +1,31 @@
-
 #include <stdio.h>
-//typedef struct{
-   // char Nombre[20] ;
-   // int dificultad;
-   // int tiempo;
-//}Datosjugadores;
+void tablero(int matriz[][24],int fila,int columna);
 int main (){
-    // Preguntamos al jugador su nombre y lo guardamos en un fichero.
-    printf("Introduzca el nombre del jugador.\n");
-    char Nombre[50];
-FILE *f;
-f= fopen("datosjugadores.txt","w");
-if(f==NULL){
-    printf("Error al abrir el fichero.\n", "w");
-    return -1;
+    int Tablero[9][9]={
+        {0,0,0,0,0,0,0,0,0,0,0}
+        {1,1,1,1,0,0,1,1,1,0,0}
+        {0,0,0,1,1,1,1,0,0,0,0}
+        {0,0,0,1,0,1,1,1,1,1,0}
+        {0,1,1,1,1,1,0,1,0,1,0}
+        {0,1,0,0,0,0,0,1,0,1,0}
+        {0,1,1,0 ,0,0,0,1,0,0,0}
+        {0,0,1,0,0,0,0,1,0,0,0}
+        {0,0,1,1,0,0,0,1,1,1,1}
+        {0,0,0,0,0,0,0,0,0,0,0}
+    }
+_Bool esblanca;//1 es casilla blanca,0 es casilla negra
 }
-else {// Si ha funcionado, comienza escritura
-fprintf(f,"%s", Nombre [50]);
-fclose(f);
-//return 0;
-}
+void tablero(int matriz[][],int fila,int columna){
+int i,j;
+int M[24][24]= {
+
+for(i = 0; i < 24; i++)
+for(j = 0; j < 24; j++)
+//Le damos a los laterales del tablero el valor de un booleano 1 que serian las casillas negras, las casillas de la matriz
+// con un valor de bool 0 es que la casilla es blanca
+    if()
 
 
-// Menu para seleccionar el nivel de dificultad
-int op;
-do{
-            printf("Seleccione el nivel de dificultad del laberinto:\n ");
-            printf("1. Facil \n");
-            printf("2. Medio \n");
-            printf("3. Medio \n");
-            printf("4. Medio \n");
 
-scanf("%i",&op);
-    switch(op)
-{
-case 1:
-printf("Dificultad facil\n");
 
-break;
-case 2:
-printf("Dificultad media");
-
-break;
-case 3:
-printf("Dificultad facil\n");
-break;
-case 4:
-printf("Salir");
-
-break;
-default:
-printf("No ha seleccionado ningun nivel de dificultad, por favor reintentelo...\n");
-break;
-}
-
-}
-while(op!=4);
-
-return 0;
 }
