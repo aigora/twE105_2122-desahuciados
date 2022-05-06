84afa7bfa9cb5328ad11c94b527c4129ef12f554
diego <79934958+diego22222@users.noreply.github.com>
2022-05-06 21:04:08 +0200

    Update main__nulo.c

--- trabajo/main__nulo.c
+++ trabajo/main__nulo.c
@@ -4,7 +4,7 @@
    // int dificultad;
    // int tiempo;
 //}Datosjugadores;
-int main (){
+//int main (){
     // Preguntamos al jugador su nombre y lo guardamos en un fichero.
     //printf("Introduzca el nombre del jugador.\n");
     //char Nombre[50];
@@ -22,40 +22,40 @@ int main (){
 
 
 // Menu para seleccionar el nivel de dificultad
-int op;
-do{
-            printf("Seleccione el nivel de dificultad del laberinto:\n ");
-            printf("1. Facil \n");
-            printf("2. Medio \n");
-            printf("3. Medio \n");
-            printf("4. Medio \n");
+//int op;
+//do{
+          //  printf("Seleccione el nivel de dificultad del laberinto:\n ");
+           // printf("1. Facil \n");
+           // printf("2. Medio \n");
+           // printf("3. Medio \n");
+           // printf("4. Medio \n");
 
-scanf("%i",&op);
-    switch(op)
-{
-case 1:
-printf("Dificultad facil\n");
+//scanf("%i",&op);
+  //  switch(op)
+//{
+//case 1:
+//printf("Dificultad facil\n");
 
-break;
-case 2:
-printf("Dificultad media");
+//break;
+//case 2:
+//printf("Dificultad media");
 
-break;
-case 3:
-printf("Dificultad facil\n");
-break;
-case 4:
-printf("Salir");
+//break;
+//case 3:
+//printf("Dificultad facil\n");
+//break;
+//case 4:
+//printf("Salir");
 
-break;
-default:
-printf("No ha seleccionado ningun nivel de dificultad, por favor reintentelo...\n");
-break;
-}
+//break;
+//default:
+//printf("No ha seleccionado ningun nivel de dificultad, por favor reintentelo...\n");
+//break;
+//}
 
-}
-while(op!=4);
+//}
+//while(op!=4);
 
-return 0;
-}
+//return 0;
+//}
 
