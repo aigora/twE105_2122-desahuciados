07b03d346dd90570ce1c06b16f578e1f7ed757b1
diego <79934958+diego22222@users.noreply.github.com>
2022-05-23 12:19:44 +0200

    Menu Beta acabado, empiezo con el tablero

--- trabajo/main__nulo.c
+++ trabajo/main__nulo.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+void tablero(int matriz[][24],int fila,int columna);
 typedef struct{
     char Nombre[20] ;
     int dificultad;
@@ -21,28 +22,28 @@ int main (){
 //}
 
 
- Menu para seleccionar el nivel de dificultad
+// Menu para seleccionar el nivel de dificultad
 int op;
 do{
             printf("Seleccione el nivel de dificultad del laberinto:\n ");
             printf("1. Facil \n");
             printf("2. Medio \n");
-            printf("3. Medio \n");
-            printf("4. Medio \n");
+            printf("3. Dificil \n");
+            printf("4. Salir \n");
 
 scanf("%i",&op);
     switch(op)
 {
 case 1:
-printf("Dificultad facil\n");
+printf("Dificultad Facil\n");
 
 break;
 case 2:
-printf("Dificultad media");
+printf("Dificultad Media");
 
 break;
 case 3:
-printf("Dificultad facil\n");
+printf("Dificultad Dificil\n");
 break;
 case 4:
 printf("Salir");
@@ -54,8 +55,39 @@ break;
 }
 
 }
-while(op!=4);
+while((op!=4)&&(op!=3)&&(op!=2)&&(op!=1));
 
 return 0;
 }
+
+
+void tablero(int matriz[][24],int fila,int columna){
+
+int i,j;
+_Bool esblanca;//1 es casilla blanca,0 es casilla negra
+
+    int Tablero[9][9]=
+    {{0,0,0,0,0,0,0,0,0,0,0},
+    {1,1,1,1,0,0,1,1,1,0,0},
+    {0,0,0,1,1,1,1,0,0,0,0},
+    {0,1,1,1,1,1,0,1,0,1,0},
+    {0,1,0,0,0,0,0,1,0,1,0},
+    {0,1,1,0,0,0,0,1,0,0,0},
+    {0,0,1,0,0,0,0,1,0,0,0},
+    {0,0,1,1,0,0,0,1,1,1,1},
+    {0,0,0,0,0,0,0,0,0,0,0}};
+
+for(i = 0; i < 9; i++){
+    for(j = 0; j < 9; j++){
+    if (j == 0)
+printf ("|\t");
+else
+printf("%d\t " ,Tablero[i][j]);
+}
+printf("| \n");
+}
+}
+
+//Le damos a los laterales del tablero el valor de un booleano 1 que serian las casillas negras, las casillas de la matriz
+// con un valor de bool 0 es que la casilla es blanca
 
